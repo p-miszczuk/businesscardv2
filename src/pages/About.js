52b9961be38e3d7087b55800b84b41213188012a
p-miszczuk <piotr.miszczuk@interia.pl>
2020-10-01 09:37:21 +0200

    About: add animation

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="about">
+    <section className="about">
       <header className="about__header">
         <span>?</span>
         <span />
@@ -15,7 +15,22 @@ const About = () => {
           </p>
         ))}
       </div>
-    </div>
+      <div className="about__stack">
+        <h2>Umiejętności i technologie</h2>
+        <div className="about__stack-content">
+          <div className="about__technologie">
+            <div className="about__technologie-name">JavaScript</div>
+            <div className="about__technologie-extra">
+              <span>Redux</span>
+              <span>Ajax</span>
+              <span>Redux 1</span>
+              <span>Ajax 1</span>
+            </div>
+            <div className="about__cover" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
@@ -33,5 +48,98 @@ const aboutContent = [
     text: `Od ponad roku pracuję w firmie React Poland z siedzibą w Gliwicach. Poszerzając swoją wiedzę korzystam z różnych kursów internetowych, youtube, forów oraz z doświadczenia moich kolegów.`
   }
 ];
+
+const stack = {
+  good: [
+    {
+      id: 1,
+      name: "HTML5",
+      img: ""
+    },
+    {
+      id: 2,
+      name: "CSS3",
+      extra: [
+        {
+          id: 1,
+          name: "SASS (SCSS)",
+          img: ""
+        }
+      ],
+      img: ""
+    },
+    {
+      id: 3,
+      name: "JavaScript",
+      extra: [
+        {
+          id: 1,
+          name: "AJAX",
+          img: ""
+        },
+        {
+          id: 2,
+          name: "jQuery",
+          img: ""
+        }
+      ],
+      img: ""
+    },
+    {
+      id: 4,
+      name: "React",
+      extra: [
+        {
+          id: 1,
+          name: "Redux",
+          img: ""
+        },
+        {
+          id: 2,
+          name: "Material UI",
+          img: ""
+        },
+        {
+          id: 3,
+          name: "Bootstrap",
+          img: ""
+        }
+      ],
+      img: ""
+    },
+    {
+      id: 5,
+      name: "Git",
+      img: ""
+    },
+    {
+      id: 6,
+      name: "RWD",
+      img: ""
+    }
+  ],
+  basic: [
+    {
+      id: 1,
+      name: "Node",
+      img: ""
+    },
+    {
+      id: 2,
+      name: "PHP",
+      img: ""
+    },
+    {
+      id: 3,
+      name: "MySql",
+      img: ""
+    },
+    {
+      id: 4,
+      name: "Photoshop",
+      img: ""
+    }
+  ]
+};
 
 export default About;
